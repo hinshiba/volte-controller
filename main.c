dc6e05bbee24649c90d3d398d868e78143018c2b
d1b26 <105423175+d1b26@users.noreply.github.com>
2024-11-05 20:43:29 +0900

    add: first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "hardware/gpio.h"
+#include "pico/multicore.h"
+#include "pico/stdlib.h"
+
+#define LED_BUILTIN 25
+
+int main() {
+    stdio_init_all();
+
+    gpio_init(LED_BUILTIN);
+    gpio_set_dir(LED_BUILTIN, GPIO_OUT);
+
+    while (true) {
+        gpio_put(LED_BUILTIN, 1);
+        sleep_ms(1000);
+        gpio_put(LED_BUILTIN, 0);
+    }
+}
